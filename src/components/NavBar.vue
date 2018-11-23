8eb9b33c41a092b322fb7ee881d36cc92a80697c
zpq <zpqwangyiyouxiang@163.com>
2018-11-23 09:24:13 +0800

    commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <van-nav-bar
+    title="标题"
+    left-text="返回"
+    right-text="按钮"
+    left-arrow>
+  </van-nav-bar>
+</template>
+
+<script>
+import { NavBar } from 'vant'
+export default {
+  name: 'NavBar',
+  components: {
+    'van-nav-bar': NavBar
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const glob = require('glob')
+
+// function resolve (dir) {
+//   return path.join(__dirname, dir)
+// }
+
+function getEntry () {
+  let pages = {}
+  let rewrites = []
+  // console.log(glob.sync('src/pages/**/main.js'))
+  glob.sync('src/pages/**/main.js').forEach(entry => {
+    const tempArr = path.dirname(entry).split('/')
+    // let tempArrLength = tempArr.length
+    let pathName = ''
+    let filename = ''
+    pathName = getPathName(tempArr, pathName, tempArr.length)
+    filename = getFilename(tempArr, filename, tempArr.length)
+    // console.log(filename)
+    const htmlPath = path.dirname(entry) + '/index.html'
+    pages[pathName] = {
+      entry,
+      filename,
+      template: htmlPath,
+      title: '一品良驹'
+    }
+    rewrites.push({ from: new RegExp('^/' + pathName + '$'), to: `/${pathName}.html` })
+  })
+  // console.log(rewrites)
+  return { pages, rewrites }
+}
+
+function getPathName (tempArr, pathName, length) {
+  let name = pathName
+  if (length >= 3) {
+    if (length > 3) {
+      if (tempArr[length - 1] !== 'index') {
+        name = name !== '' ? tempArr[length - 1] + '/' + name : tempArr[length - 1]
+      }
+    } else {
+      name = name !== '' ? tempArr[length - 1] + '/' + name : tempArr[length - 1]
+    }
+    length = length - 1
+    return getPathName(tempArr, name, length)
+  } else {
+    return name
+  }
+}
+
+function getFilename (tempArr, filename, length) {
+  let name = filename
+  if (length >= 3) {
+    name = name !== '' ? tempArr[length - 1] + '/' + name : tempArr[length - 1]
+    length = length - 1
+    return getFilename(tempArr, name, length)
+  } else {
+    return name + '.html'
+  }
+}
+
+const entryConfig = getEntry()
+
+module.exports = {
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': []
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: false
+    }
+  },
+  pages: entryConfig.pages,
+  devServer: {
+    historyApiFallback: {
+      rewrites: entryConfig.rewrites
+    }
+  }
+}
